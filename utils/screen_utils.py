e606aee9724f6bb5cbbad422e53b569511a7e203
test <test@test.com>
2024-07-26 09:59:10 +0800

    提交封装类：截图、显示等待

---
+++ utils/screen_utils.py
@@ -0,0 +1,15 @@
+
+'''
+封装截图方法，并结合allure报告
+'''
+import time
+import allure
+
+def get_screen(driver):
+    #todo:手动创建images目录
+    timestamp = int(time.time())
+    image_path = f"./images/image_{timestamp}.PNG"
+    #截图
+    driver.save_screenshot(image_path)
+    #结合allure报告
+    allure.attach.file(image_path,name="picture",attachment_type=allure.attachment_type.PNG)
